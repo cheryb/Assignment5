d58b8f654871383d439f3c77ee40171f1b3e28d8
cheryb <106184295+cheryb@users.noreply.github.com>
2022-07-29 20:37:52 -0400

    Update and rename lab5.py to lab5point3_5_6_7.py

--- lab5point3_5_6_7.py
+++ lab5point3_5_6_7.py
@@ -92,31 +92,6 @@ import collections
 database = sqlite3.connect("assignment5.db")
 cursor = database.cursor()
 
-# 1 • Add/remove course from semester schedule (based on course ID number).
-
-def add_remove_course_from_schedule():
-    print("You have selected option 1. Add/remove course from semester schedule (based on course ID number).")
-     choice2 = input("Do you want to add (1) or remove(2) this course from your schedule: ")
-    while choice2 == 1 or 2:       
-        choice2 = int(choice2)
-        if choice2 == 1:
-            course = input("Enter course ID number: ")
-            course = int(course)
-            cursor.execute("""SELECT crn FROM COURSE WHERE crn=?""", [course])
-            for row in iter(cursor.fetchone, None):
-                print(row)                
-        elif choice2 == 2:
-            course = input("Enter course ID number: ")
-            course = int(course)
-            cursor.execute("""SELECT crn FROM COURSE WHERE crn=?""", [course])
-            for row in iter(cursor.fetchone, None):
-                print(row)
-
-# 2 • Assemble and print course roster (instructor).
-
-def print_course():
-    print("You have selected option 2. Assemble and print course roster (instructor).")
-
 # 3 • Add/remove courses from the system (admin).
 
 def add_remove_course_from_system():
@@ -124,28 +99,6 @@ def add_remove_course_from_system():
     cursor.execute("""INSERT INTO COURSE VALUES(23300, 'DATA STRUCTURES', 'BSCS', 1400, 'MW', 'SPRING', 2022, 4);""")
     cursor.execute("""DELETE FROM COURSE WHERE title = 'CALCULUS 3';""")
 
-# 4 • Log-in, log-out (all users).
-
-def log_in_log_out():
-    print("You have selected option 4. Log-in, log-out (all users).")
-    choice = input("Are you a student (1), instructor(2) or admin(3)? ")
-    choice = int(choice) 
-    name = input("Enter email: ")
-    if choice == 1:
-        cursor.execute("""SELECT email FROM STUDENT WHERE email=?""", [name])
-        for row in iter(cursor.fetchone, None):
-            print(row)
-            print("You are now logged in")
-    elif choice == 2:
-        cursor.execute("""SELECT email FROM INSTRUCTOR WHERE email=?""", [name])
-        for row in iter(cursor.fetchone, None):
-            print(row)
-            print("You are now logged in")
-    elif choice == 3:
-        cursor.execute("""SELECT email FROM ADMIN WHERE email=?""", [name])
-        for row in iter(cursor.fetchone, None):
-            print(row)
-            print("You are now logged in")
 
 # 5 • Search all courses (all users) .
 
